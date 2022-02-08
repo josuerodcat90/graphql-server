d621944c9cd1dfdb96dfe64322760c0db9914a2e
sergio <shernandez@abbadminabbanza.onmicrosoft.com>
2022-02-08 19:52:49 +0100

    Class 5 completed!, and added all resolver so json-server API.

--- index.js
+++ index.js
@@ -1,49 +1,7 @@
 import { gql } from 'apollo-server';
 import { ApolloServer, UserInputError } from 'apollo-server';
 import { v4 } from 'uuid';
-
-const persons = [
-	{
-		id: '0298ad2d-b4a8-4f7b-a381-c96dd89da726',
-		name: 'Loise Ivett',
-		age: 30,
-		phone: null,
-		street: '5213 Vera Center',
-		city: 'Jablonec nad Nisou',
-	},
-	{
-		id: '14d34151-9a08-48fb-bff8-166f3d9d5dc6',
-		name: 'Ines Colebeck',
-		age: 8,
-		phone: '(989) 9732428',
-		street: '11578 Bowman Park',
-		city: 'Loo',
-	},
-	{
-		id: 'e667f437-11b8-4f6a-8a4a-ce9b302366c5',
-		name: 'Derward Cullity',
-		age: 19,
-		phone: '(255) 1988827',
-		street: '47 Mayer Avenue',
-		city: 'Santa Maria',
-	},
-	{
-		id: '2259f33a-bc31-40cc-8541-141680b852a9',
-		name: 'Lonee Wythill',
-		age: 16,
-		phone: null,
-		street: '2 Rusk Way',
-		city: 'Malahide',
-	},
-	{
-		id: 'f7fa72b0-969b-4d12-801f-226c2c1d697d',
-		name: 'Flora Cato',
-		age: 34,
-		phone: '(324) 7002749',
-		street: '979 Morrow Hill',
-		city: 'Xinglongjie',
-	},
-];
+import axios from 'axios';
 
 const typeDefs = gql`
 	enum YesNo {
@@ -63,7 +21,7 @@ const typeDefs = gql`
 		phone: String
 		street: String!
 		address: Address!
-		canDrink: Boolean!
+		canDrink: String!
 		check: String!
 		city: String
 	}
@@ -89,22 +47,46 @@ const typeDefs = gql`
 
 const resolvers = {
 	Query: {
-		personCount: () => persons.length,
-		allPersons: (root, args) => {
-			if (!args.hasPhone) return persons;
+		personCount: async () => {
+			const { data: persons } = await axios.get(
+				'http://localhost:3000/persons'
+			);
+
+			return persons.length;
+		},
+		allPersons: async (root, args) => {
+			const { data: persons } = await axios.get(
+				'http://localhost:3000/persons'
+			);
+
+			const { hasPhone } = args;
+
+			if (!hasPhone) return persons;
 
 			const byPhone = (person) =>
-				args.hasPhone === 'YES' ? person.phone : !person.phone;
+				hasPhone === 'YES' ? person.phone : !person.phone;
 
 			return persons.filter(byPhone);
 		},
-		findPerson: (root, args) => {
+		findPerson: async (root, args) => {
 			const { name } = args;
-			return persons.find((person) => person.name === name);
+
+			const person = await axios
+				.get(`http://localhost:3000/persons?name=${name}`)
+				.then((res) => res.data[0])
+				.catch((err) => {
+					throw new UserInputError(err.message, {
+						invalidArgs: args,
+					});
+				});
+
+			return person;
 		},
 	},
 	Person: {
-		canDrink: (person) => person.age >= 18,
+		canDrink: (person) => {
+			return person.age >= 18 ? 'YES' : 'NOPE';
+		},
 		address: (person) => {
 			return {
 				street: person.street,
@@ -114,11 +96,20 @@ const resolvers = {
 		check: () => 'LOL',
 	},
 	Mutation: {
-		addPerson: (root, args) => {
+		addPerson: async (root, args) => {
 			const { name, age, phone, street, city } = args;
 
-			if (persons.find((person) => person.name === name)) {
-				throw new UserInputError('Name must be unique', {
+			const person = await axios
+				.get(`http://localhost:3000/persons?name=${name}`)
+				.then((res) => res.data[0])
+				.catch((err) => {
+					throw new UserInputError(err.message, {
+						invalidArgs: args,
+					});
+				});
+
+			if (person) {
+				throw new UserInputError('The user already exist!', {
 					invalidArgs: name,
 				});
 			}
@@ -132,24 +123,31 @@ const resolvers = {
 				city,
 			};
 
-			persons.push(newPerson);
+			await axios.post('http://localhost:3000/persons', newPerson);
 
 			return newPerson;
 		},
-		editNumber: (root, args) => {
+		editNumber: async (root, args) => {
 			const { name, phone } = args;
-			const personIndex = persons.findIndex((person) => person.name === name);
+			const { data: persons } = await axios.get(
+				'http://localhost:3000/persons'
+			);
 
-			if (personIndex === -1) {
+			const person = persons.find((person) => person.name === name);
+
+			if (!person) {
 				throw new UserInputError('Person not found', {
 					invalidArgs: name,
 				});
 			}
 
-			const person = persons[personIndex];
-
 			const updatedPerson = { ...person, phone };
-			persons[personIndex] = updatedPerson;
+
+			await axios.put(`http://localhost:3000/persons/${person.id}/`, {
+				...person,
+				phone,
+			});
+			// persons[personIndex] = updatedPerson;
 
 			return updatedPerson;
 		},
